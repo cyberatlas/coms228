30b9678faa882c98d1bc73f6d4b750d6d5719e17
Alex Stevenson <ruski550@gmail.com>
2017-09-08 23:06:52 -0500

    Finished the intial writing of the code...Now to go back and test/fix everything....

--- edu/iastate/cs228/hw1/ProteinSequence.java
+++ edu/iastate/cs228/hw1/ProteinSequence.java
@@ -14,12 +14,22 @@ public class ProteinSequence extends Sequence
       if!(isValidLetter(psarr[i])){
         throw IllegalArgumentException("Invalid sequence letter for class" + this.getClass());
       }
+      //
+      //TODO I have no idea if this is right or not, I am assuming that they want the psarr to be instead of seqarr if this is true
+      seqarr[i]=psarr[i];
+
     }
   }
 
   @Override
   public boolean isValidLetter(char aa)
   {
-    // TODO
+
+    aa.toUpperCase();
+    //Returns true if character arrgument is equal to one of the letters not in the set
+    if !(aa =='B' || aa=='J'||aa=='O'|| aa== 'U' || aa =='X' || a=='Z'){
+      return true;
+    }
+    return false;
   }
 }
